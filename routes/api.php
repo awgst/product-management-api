92b6b739820aa85fa08dfadf6ce359e9c323fef8
Awang Trisakti <awang@jakpat.net>
2023-03-21 08:38:36 +0700

    Update category

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,6 @@ Route::group(['prefix' => 'v1'], function () {
         Route::get('/', [CategoryController::class, 'index']);
         Route::get('/{id}', [CategoryController::class, 'show']);
         Route::post('/', [CategoryController::class, 'store']);
+        Route::put('/{id}', [CategoryController::class, 'update']);
     });
 });
